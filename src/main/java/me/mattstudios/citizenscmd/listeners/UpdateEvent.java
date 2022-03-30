4cebc1619364232564063e0799f6dc7e9b6e38a3
HexedHero <6012891+HexedHero@users.noreply.github.com>
2022-03-30 17:29:41 +0100

    Change to the complete URL

--- src/main/java/me/mattstudios/citizenscmd/listeners/UpdateEvent.java
+++ src/main/java/me/mattstudios/citizenscmd/listeners/UpdateEvent.java
@@ -58,8 +58,8 @@ public class UpdateEvent implements Listener {
         builder.append(
                 Component.text()
                 .append(plugin.getLang().getMessage(Messages.DOWNLOAD_AT))
-                .append(text(" spigotmc.org/resources/30224/"))
-                .clickEvent(ClickEvent.openUrl("https://spigotmc.org/resources/30224/"))
+                .append(text(" https://www.spigotmc.org/resources/30224/"))
+                .clickEvent(ClickEvent.openUrl("https://www.spigotmc.org/resources/30224/"))
                 .build()
                 );
 
